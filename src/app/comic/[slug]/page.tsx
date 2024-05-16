4a7b6a4cb73e04213c850d57b368af42557d19f4
enzocomics <enzo@cheerupemokid.com>
2024-05-16 01:06:40 -0700

    WIP: generating routes for comic pages

--- src/app/comic/[slug]/page.tsx
+++ src/app/comic/[slug]/page.tsx
@@ -1,12 +1,25 @@
 import Comic from "@ui/comic-component"
 import fs from "fs"
 
+const path = "public/comic/img"
+const comics = fs.readdirSync(path)
+
 /** ------------------------------------------------ **
  * Comic Route
  ** ------------------------------------------------ **/
-export default function Page() {
+export default function Page({
+	params
+}: {
+	params: { slug: string }
+}) {
+
+	// TODO: Check if the current page url is correct
+	// - compare it to the list of comic images
+	// - Throw 404 if it doesn't find a comic
+
 	return <>
 		<Comic />
+		<h1>{params.slug}</h1>
 	</>
 }
 
@@ -16,11 +29,12 @@ export default function Page() {
  ** ------------------------------------------------ **/
 
 export function generateStaticParams() {
-	const path = "public/comic/img"
-	// Get a list of all the comics in the public/comics folder
-	const comics = fs.readdirSync(path)
+	// Get a list of all the comics in the comic images folder
+
 	// Generate a static segment for every comic image
 	return comics.map((filename) => ({
+		// Remove the file extension
 		slug: filename
+		//slug: filename.replace(/(\.[^/.]+)+$/, "")
 	}))
 }
