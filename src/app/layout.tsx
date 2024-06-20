5358845d3b2536fc5b788b053e1f5577e73a2d4e
enzocomics <enzo@cheerupemokid.com>
2024-06-19 23:08:45 -0700

    working on header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,17 @@ export default function RootLayout({
 	return (
 		<html lang="en" className={`${copy.variable} ${display.variable}`}>
 			<body>
-				<header className="bg-red">
-					<Image src="/img/logo.png" alt="" width="716" height="80" />
+
+				<header className="p-5 bg-red-900 text-neutral-200 text-center text-sm">
+					<div className="max-w-4xl mx-auto">
+						<Image className="pb-5 mx-auto" src="/img/logo.png" alt="" width="716" height="80" />
+						<p>&copy; Rooster Teeth Productions, LLC   |   Written by Griffon Ramsey, Illustrated by Luke McKay, and Edited by Geoff Ramsey<br />
+							RoosterTeethComics.com is an archive of the series of webcomics about the staff at Rooster Teeth that ran from 2006 to 2011</p>
+					</div>
 				</header>
-				{children}
+				<main className="p-10 max-w-4xl mx-auto">
+					{children}
+				</main>
 			</body>
 		</html>
 	)
