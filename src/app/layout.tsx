6e128701e4aed33f961b3af769972c6dcd4abd86
enzocomics <enzo@cheerupemokid.com>
2024-06-24 08:08:17 -0700

    added link to header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import "@styles/global.css"
 import { copy, display } from '@styles/fonts'
 import Image from "next/image"
+import Link from "next/link"
 
 /** ------------------------------------------------ **
  * Root Layout 
@@ -18,11 +19,11 @@ export default function RootLayout({
 			<body>
 
 				<header className="p-5 bg-red-900 text-neutral-200 text-center text-sm">
-					<div className="max-w-4xl mx-auto">
+					<Link className="max-w-4xl mx-auto" href="/">
 						<Image className="pb-5 mx-auto" src="/img/logo.png" alt="" width="716" height="80" />
 						<p>&copy; Rooster Teeth Productions, LLC   |   Written by Griffon Ramsey, Illustrated by Luke McKay, and Edited by Geoff Ramsey<br />
 							RoosterTeethComics.com is an archive of the series of webcomics about the staff at Rooster Teeth that ran from 2006 to 2011</p>
-					</div>
+					</Link>
 				</header>
 				<main className="p-10 max-w-4xl mx-auto">
 					{children}
