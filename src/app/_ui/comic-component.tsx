8861a7329a50c2f7fb7a03ca8a0cc4c537edfb0d
enzocomics <enzo@cheerupemokid.com>
2024-06-26 22:18:30 -0700

    WIP comic navigation

--- src/app/_ui/comic-component.tsx
+++ src/app/_ui/comic-component.tsx
@@ -21,7 +21,8 @@ export default function Comic({
 	const comics = fs.readdirSync(path)
 
 	// Find the index of the current comic
-	let comicIndex = comics.findIndex(comic => comic.includes(slug))
+	let comicIndex
+	comicIndex = comics.findIndex(comic => comic.includes(slug))
 
 	// The homepage won't have a slug, so just display the latest comic
 	let thisComic
@@ -34,10 +35,21 @@ export default function Comic({
 	const comicDate = thisComic.substring(0, 10)
 	const comicDateFormatted = dayjs(comicDate).format("MMMM D, YYYY")
 
+	// Find the indices of the previous and next comic, based on the current comic's Index
+	const prevComic = comics[comicIndex - 1]
+	const nextComic = comics[comicIndex + 1]
+
+	console.log('---')
+	console.log(prevComic, comicIndex - 1)
+	console.log(thisComic, comicIndex)
+	console.log(nextComic, comicIndex + 1)
+
+
+
 	return <>
 		<h1 className="text-center mb-8">{comicDateFormatted}</h1>
 
-		<ComicNav slug={slug} />
+		<ComicNav slug={slug} prevComic={prevComic} nextComic={nextComic} />
 
 		<Image src={`/comic/img/${thisComic}`} alt="" width="600" height="1000" className="mx-auto" />
 
@@ -46,16 +58,20 @@ export default function Comic({
 }
 
 function ComicNav({
+	prevComic,
+	nextComic,
 	slug,
 }: {
+	prevComic?: string
+	nextComic?: string
 	slug: string
 }) {
 	return <>
 		<nav className="grid grid-flow-col w-[500px] mx-auto my-6 text-center font-display text-3xl">
 			<ComicLink href="#">&laquo; First</ComicLink>
-			<ComicLink href="#">&lt; Prev</ComicLink>
+			<ComicLink href={prevComic}>&lt; Prev</ComicLink>
 			<ComicLink href="#">Random</ComicLink>
-			<ComicLink href="#">Next &gt;</ComicLink>
+			<ComicLink href={nextComic}>Next &gt;</ComicLink>
 			<ComicLink href="#">Last &raquo;</ComicLink>
 		</nav>
 	</>
@@ -66,9 +82,11 @@ function ComicLink({
 	href
 }: {
 	children: ReactNode,
-	href: URL | "#"
+	href?: string
 }) {
-	return <>
-		<Link href={href} className="hover:text-red-900">{children}</Link>
-	</>
+	if (href === undefined) {
+		return <span className="opacity-50">{children}</span>
+	} else {
+		return <Link href={href} className="hover:text-red-900">{children}</Link>
+	}
 }
