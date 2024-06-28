94116881784df0e85e96b3bbe053d7a127a360ae
enzocomics <enzo@cheerupemokid.com>
2024-06-27 22:10:39 -0700

    cleaned up main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
 import Comic from "@ui/comic-component"
-import fs from "fs"
 
 /** ------------------------------------------------ **
  * Homepage **REQUIRED**
  ** ------------------------------------------------ **/
 export default function Page() {
-	// Get the paths of the comics folder
-	const path = "public/comic/img"
-	const comics = fs.readdirSync(path)
-	const lastComic = comics[comics.length - 1]
-
 	return <>
 		<Comic slug="latest" />
 	</>
