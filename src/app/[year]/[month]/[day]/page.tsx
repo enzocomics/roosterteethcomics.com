33f8199ae81a5ace8dfb27f99c9f54381cb8a27f
Enzo <enzo@cheerupemokid.com>
2025-10-12 15:53:17 -0700

    corrected param await error

--- src/app/[year]/[month]/[day]/page.tsx
+++ src/app/[year]/[month]/[day]/page.tsx
@@ -21,9 +21,10 @@ export async function generateMetadata({
 	const path = "public/comic/img"
 	const comics = fs.readdirSync(path)
 
-	const year = params.year
-	const month = params.month
-	const day = params.day
+	const { year, month, day } = await params
+	// const year = params.year
+	// const month = params.month
+	// const day = params.day
 
 	// Retrieve the current comic, with fallback on the homepage
 	const comicDate = year && month && day ? year + "_" + month + "_" + day : undefined
@@ -39,15 +40,18 @@ export async function generateMetadata({
 /** ------------------------------------------------ **
  * Comic Route
  ** ------------------------------------------------ **/
-export default function Page({
+export default async function Page({
 	params
 }: {
 	//params: { page: number }
 	params: { year: number, month: number, day: number }
 }) {
 
+	const { year, month, day } = await params
+
+
 	return <>
-		<Comic year={params.year} month={params.month} day={params.day} />
+		<Comic year={year} month={month} day={day} />
 	</>
 }
 
